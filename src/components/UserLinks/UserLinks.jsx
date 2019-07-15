46e56bc77813609eb1de55f6dd6dc6d5064229bb
Seth Lewis <lewiscatenary@gmail.com>
2019-07-14 20:48:06 -0500

    fixed button/link bug

--- src/components/UserLinks/UserLinks.jsx
+++ src/components/UserLinks/UserLinks.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
+// import {Link} from 'gatsby'
 import "./UserLinks.css";
 
 class UserLinks extends Component {
   getLinkElements() {
-    const { userLinks } = this.props.config;
+    const { config: {userLinks} } = this.props;
     const { labeled } = this.props;
+    // handleClick = () => {
+    //   </Link>
+    // }
     return userLinks.map(link => (
-      <button key={link.label} href={link.url}>
-        {labeled ? link.label : ""}
-      </button>
+      <a href={link.url}>
+        <button type='button' key={link.label} href={link.url}>
+          {labeled ? link.label : ""}
+        </button>
+      </a>
     ));
   }
 
   render() {
-    const { userLinks } = this.props.config;
+    const { config: {userLinks} } = this.props;
     if (!userLinks) {
       return null;
     }
