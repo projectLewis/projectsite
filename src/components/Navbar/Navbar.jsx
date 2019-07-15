351ba60dc9022ad0d2480aced05e2b1f85674e87
Seth Lewis <lewiscatenary@gmail.com>
2019-07-15 02:49:18 -0500

    Added regex for blog active links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,18 @@ import {Link} from 'gatsby';
 import { Menu, Segment } from 'semantic-ui-react';
 import './Navbar.css';
 
+const handleLocation = local => {
+// const regEx = `${/\/blog\/lil-sample-test/}`
+  if (/\/blog\/.+/.test(local)) {
+    return '/blog'
+  } if (local === '/blog-list') {
+    return '/blog'
+  }if (/\/portfolio\/.+/.test(local)) {
+    return '/portfolio'
+  }
+    return null
+}
+
 const Navbar = ({location}) => (
   <Segment inverted>
     <Menu inverted pointing secondary>
@@ -10,11 +22,11 @@ const Navbar = ({location}) => (
         <Link to="/">Home</Link> 
       </Menu.Item>
 
-      <Menu.Item active={location === "/portfolio" && 'portfolio'} name='portfolio'>
+      <Menu.Item active={location === "/portfolio" && 'home'} name='portfolio'>
         <Link to="portfolio">Portfolio</Link>
       </Menu.Item>
 
-      <Menu.Item active={location === "/blog-list" && 'blog'} name='blog'>
+      <Menu.Item active={handleLocation(location)} name='blog'>
         <Link to="/blog-list">Blog</Link>
       </Menu.Item>
 
