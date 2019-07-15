66fac1a9b9a4bcff686a424ac837bf31ab81e106
Seth Lewis <lewiscatenary@gmail.com>
2019-07-14 21:13:53 -0500

    removed blog list from index page in preparation for a general home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,13 +8,12 @@ import config from "../../data/SiteConfig";
 
 class Index extends React.Component {
   render() {
-    const postEdges = this.props.data.allMarkdownRemark.edges;
     return (
       <Layout>
         <div className="index-container">
           <Helmet title={config.siteTitle} />
           <SEO />
-          <PostListing postEdges={postEdges} />
+          Home Page
         </div>
       </Layout>
     );
@@ -22,30 +21,3 @@ class Index extends React.Component {
 }
 
 export default Index;
-
-/* eslint no-undef: "off" */
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(
-      limit: 2000
-      sort: { fields: [fields___date], order: DESC }
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-            date
-          }
-          excerpt
-          timeToRead
-          frontmatter {
-            title
-            tags
-            cover
-            date
-          }
-        }
-      }
-    }
-  }
-`;
