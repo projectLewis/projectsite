6677cd439725e46b21a65550f5c002de632fb60c
Seth Lewis <lewiscatenary@gmail.com>
2019-07-14 21:40:34 -0500

    removed unused imports

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Helmet from "react-helmet";
-import { graphql } from "gatsby";
 import Layout from "../layout";
-import PostListing from "../components/PostListing/PostListing";
 import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
 
@@ -10,6 +8,7 @@ class Index extends React.Component {
   render() {
     return (
       <Layout>
+        {/* change classname */}
         <div className="index-container">
           <Helmet title={config.siteTitle} />
           <SEO />
