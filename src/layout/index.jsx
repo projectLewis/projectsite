7fdc5c6271362ef4fd45c9f037e4e5d0ec970af7
Seth Lewis <lewiscatenary@gmail.com>
2019-07-17 12:29:54 -0500

    changed index.css to module

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Helmet from "react-helmet";
+// config
 import config from "../../data/SiteConfig";
-import "./index.css";
-import 'semantic-ui-css/semantic.min.css'
+// components
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
+// styles
+import 'semantic-ui-css/semantic.min.css';
+import style from "./index.module.css";
 
 export default class MainLayout extends React.Component {
   render() {
     const { children } = this.props;
     return (
-      <div id='viewContainer'>
+      <div className={style.viewContainer}>
         <Helmet>
           <meta name="description" content={config.siteDescription} />
         </Helmet>
