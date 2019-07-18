e2660820832373f0a33acf1af35386f2ab3fdbd1
Seth Lewis <lewiscatenary@gmail.com>
2019-07-18 17:16:34 -0500

    formatted portfolio page

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,12 +3,21 @@ import ProjectFeatured from './ProjectFeatured';
 import ProjectCard from './ProjectCard';
 import style from './Portfolio.module.css'
 
+
 const Portfolio = () => {
   return (
     <div className={style.portfolioContainer}>
-      <ProjectFeatured />
-      <div className={style.cardsContainer}>
+      <ProjectFeatured className={style.portfolioMain} />
+      <div className={style.wideCard}>
         <ProjectCard />
+      </div>
+      <div className={style.thinCard}>
+        <ProjectCard />
+      </div>
+      <div className={style.thinCard}>
+        <ProjectCard />
+      </div>
+      <div className={style.wideCard}>
         <ProjectCard />
       </div>
     </div>
