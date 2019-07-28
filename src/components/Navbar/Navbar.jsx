390e6146bd2b74899409ecd0884170fe88774f97
Seth Lewis <lewiscatenary@gmail.com>
2019-07-28 16:54:06 -0500

    made navbar thinner

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,17 +16,17 @@ const Navbar = () => {
     <Segment inverted style={{boxShadow: "-5px 1px 9px 1px green", padding: '8px'}}>
       <Menu inverted pointing secondary className={style.navContainer}>
         <Menu.Item name='home'>
-          <Link activeClassName="active item" to="/"><img alt='project lewis logo' className={style.navlogo} src={logo} style={{marginTop: '-15px', marginBottom: '-10px'}} /></Link> 
+          <Link to="/"><img alt='project lewis logo' className={style.navlogo} src={logo} style={{marginTop: '-15px', marginBottom: '-10px'}} /></Link> 
         </Menu.Item>
-        <Menu.Item style={{alignSelf: 'center'}} name='portfolio'>
+        <Menu.Item style={{alignSelf: 'center', marginTop: '-15px', marginBottom: '-10px'}} name='portfolio'>
           <Link activeClassName="active item" to="/portfolio">Portfolio</Link>
         </Menu.Item>
 
-        <Menu.Item style={{alignSelf: 'center'}} active={blogActive} name='blog'>
+        <Menu.Item style={{alignSelf: 'center', marginTop: '-15px', marginBottom: '-10px'}} active={blogActive} name='blog'>
           <Link activeClassName="active item" to="/blog-list">Blog</Link>
         </Menu.Item>
 
-        <Menu.Item style={{alignSelf: 'center'}} name='about'>
+        <Menu.Item style={{alignSelf: 'center', marginTop: '-15px', marginBottom: '-10px'}} name='about'>
           <Link activeClassName="active item" to="/about">About</Link>
         </Menu.Item>
       </Menu>
