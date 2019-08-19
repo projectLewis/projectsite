267080e9a1d28a9fdcf0894fffb5f76033ccac98
Seth Lewis <lewiscatenary@gmail.com>
2019-08-19 12:01:44 -0500

    updated home text positioning

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,14 +8,14 @@ class About extends Component {
     return (
       <div className={style.about}>
         <div className={style.textWrapper}>
-          <h1 className={style.Headline}>Seth Lewis</h1>
           <img className={style.headshot} alt="headshot of seth" src={sethHeadshot} />
+          <h1 className={style.Headline}>Seth Lewis</h1>
+          <h3>Software Engineer</h3>
           <h5>
             <a title="linkedin" alt="linkedin" href="https://linkedin.com/in/sethlewis2/"><Icon color="blue" size="large" name="linkedin" /></a>
             <a title="github" alt="github" href="www.github.com/projectlewis"><Icon size="large" name="github" /></a>
             <a title="twitter" alt="twitter" href="www.twitter.com/projectlewis_"><Icon size="large" color="blue" inverted name="twitter" /></a>
           </h5>
-          <h3>Software Engineer</h3>
           <p>Before becoming a developer, I spent 9 years in the United States Air Force. Most of my time in the Air Force was spent traveling the world on cargo airplanes. In particular I had the pleasure of supporting the National Science Foundation in Antarctica on multiple occasions. Starting my appreciation for penguins.</p>
 
           <p>These days, I take the work ethic I got from the Air Force, and the dedication and self study it takes to be an aviator and apply them to learning the newest technologies. Feel free to take a look at my portfolio or github page to see what I've been up to.</p>
